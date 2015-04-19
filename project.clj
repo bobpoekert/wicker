ecc8d9d780c73321ab1f58ee964d3eda21e0f4e5
Bob Poekert <bob@poekert.com>
2015-04-19 14:49:27 -0700

    remove fressian, use ConcurrentHashMap for global crawl state

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-beta1"]
-                 [factual/durable-queue "0.1.3"]
+                 [factual/durable-queue "0.1.3" :exclusions [manifold/manifold byte-streams/byte-streams]]
                  [aleph "0.4.0-beta3"]
-                 [org.jsoup/jsoup "1.8.2"]
-                 [org.clojure/data.fressian "0.2.0"]])
+                 [org.jsoup/jsoup "1.8.2"]])
--- src/cheap/hella/wicker/queue.clj
+++ src/cheap/hella/wicker/queue.clj
@@ -1,15 +1,15 @@
 (ns cheap.hella.wicker.queue
   (import [java.util.concurrent LinkedBlockingQueue BlockingQueue]
           [java.net URI]
-          [java.util WeakHashMap])
+          [java.util.concurrent ConcurrentHashMap])
   (require [aleph.http :as http]
            [manifold.deferred :as d]
-           [durable-queue :as q]
-           [clojure.data.fressian :as fressian]))
+           [durable-queue :as q]))
 
 (defrecord CrawlContext [
   url-queue ^BlockingQueue
   output-queue
+  global-state
   crawl-policy
   failure-policy
   crawl-thread
@@ -19,19 +19,17 @@
 (def default-queue-length 100)
 
 (defn default-crawl-policy
-  [context global-state job]
+  [context job]
   (let [current-time (System/currentTimeMillis)]
     {:crawl? (or (not (:not-before job)) (< (:not-before job) current-time))
-     :global-state global-state 
      :job (->
               job
               (assoc :crawl-time current-time)
-              (assoc :n-tries (inc (or (:n-tries row) 0))))}))
+              (assoc :n-tries (inc (or (:n-tries job) 0))))}))
 
 (defn default-failure-policy
-  [context global-state job result]
+  [context job result]
   {:crawl? (< (:n-tries job) 10)
-   :global-state global-state
    :job job})
 
 (defn ->CrawlContext
@@ -39,6 +37,7 @@
     (CrawlContext.
       (q/queues queue-path {})
       (LinkedBlockingQueue. default-queue-length)
+      (ConcurrentHashMap.)
       crawl-policy failure-policy
       nil
       (atom nil)))
@@ -52,11 +51,11 @@
   URI
   (handle-output! [this context]
     (if (.isAbsolute this)
-      (q/put! (:url-queue context) :url (fressian/write {:uri (.toString this)}))
+      (q/put! (:url-queue context) :url {:uri (.toString this)})
       (throw (IllegalArgumentException. (format "URIs to crawl must be absolute (on %s)" (.toString this))))))
   URIFetch
   (handle-output! [this context]
-    (q/put (:url-queue context) :url (fressian/write this)))
+    (q/put! (:url-queue context) :url this))
   Object
   (handle-output! [this context]
     (.put ^BlockingQueue (:output-queue context) this)))
@@ -92,26 +91,26 @@
         (cons v (queue-seq q))))))
 
 (defn handle-failed-job!
-  [^CrawlContext context global-state job result]
+  [^CrawlContext context job result]
   ;; we're not using q/retry! because we want to add extra state to the job
   (try
-    (let [failure-result ((:failure-policy context) context global-state result)]
+    (let [failure-result ((:failure-policy context) context job result)]
       (if (:crawl? failure-result)
-        (q/put! (:url-queue context) :url (fressian/write (:job failure-result)))))
+        (q/put! (:url-queue context) :url (:job failure-result))))
     (finally
       (q/complete! (:url-queue context) job))))
 
 (defn crawler
   [^CrawlContext context]
   (let [{:keys [url-queue stop-condition crawl-policy failure-policy]} context]
-    (loop [global-state {:crawl-start-time (System/currentTimeMillis)}]
+    (loop []
       (if (realized? stop-condition)
         nil
         (let [next-job (q/take! url-queue :url)
               next-uri (to-uri @next-job)
-              policy-result (crawl-policy context global-state @next-job)]
+              policy-result (crawl-policy context @next-job)]
           (if-not (:crawl? policy-result)
-            (handle-output! policy-result)
+            (handle-output! policy-result context)
             (d/on-realized
               (http-request next-uri)
               (fn [result] ;; success
@@ -121,10 +120,10 @@
                     (q/complete! url-queue next-job))
                   (catch Throwable e
                     (do
-                      (handle-failed-job! context global-state next-job (assoc result :handle-output-exception e))
+                      (handle-failed-job! context next-job (assoc result :handle-output-exception e))
                       (throw e)))))
               (fn [result] ;; failure
-                (handle-failed-job! context global-state next-job result))))
+                (handle-failed-job! context next-job result))))
           (recur))))))
 
 (defn start-crawl-thread!
