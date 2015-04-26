fb62073dc06bcad8acd1a3433c6a9aee81a5b0c8
Bob Poekert <bob@poekert.com>
2015-04-25 18:16:29 -0700

    remove unused to-map function

--- src/cheap/hella/wicker/util.clj
+++ src/cheap/hella/wicker/util.clj
@@ -19,43 +19,42 @@
         reflection-data (reflect/reflect input-type)
         refmethods (filter #(= (type %) clojure.reflect.Method) (:members reflection-data))
         get-methods (filter #(and (= (:parameter-types %) []) ((:flags %) :public) (.startsWith (name (:name %)) "get")) refmethods)]
-    `(let [to-map# (fn [~inp-sym] ~(into {} (for [m get-methods] [(method-name-to-key (:name m)) `(. ~inp-sym ~(:name m))])))]
-      (defn ~nom [~inp-sym]
-        (proxy [clojure.lang.APersistentMap] []
-          (containsKey [k#]
-            (case k#
-              ~@(mapcat (fn [method] [(method-name-to-key (:name method)) true]) get-methods)
-              false))
-          (entryAt [k#]
-            (case k#
-              ~@(mapcat
-                (fn [method]
-                  (let [n (method-name-to-key (:name method))]
-                    [n `(clojure.lang.MapEntry. ~n (. ~inp-sym ~(:name method)))])) get-methods)
-              nil))
-          (valAt [k#]
-            (case k#
-              ~@(mapcat
-                (fn [method]
-                  (let [n (method-name-to-key (:name method))]
-                    [n `(. ~inp-sym ~(:name method))]))
-                get-methods)
-              nil))
-          (seq []
-            (seq ~(vec
-              (for [method get-methods]
-                `(clojure.lang.MapEntry. ~(method-name-to-key (:name method)) (. ~inp-sym ~(:name method)))))))
-          (entrySet []
-            ~(into #{}
-              (for [method get-methods]
-                `(clojure.lang.MapEntry. ~(method-name-to-key (:name method)) (. ~inp-sym ~(:name method))))))
-          (keySet []
-            ~(into #{} (for [method get-methods] (method-name-to-key (:name method)))))
-          (values []
-            ~(->>
-              (for [method get-methods]
-                `(. ~inp-sym ~(:name method)))
-              (vec))))))))
+    `(defn ~nom [~inp-sym]
+      (proxy [clojure.lang.APersistentMap] []
+        (containsKey [k#]
+          (case k#
+            ~@(mapcat (fn [method] [(method-name-to-key (:name method)) true]) get-methods)
+            false))
+        (entryAt [k#]
+          (case k#
+            ~@(mapcat
+              (fn [method]
+                (let [n (method-name-to-key (:name method))]
+                  [n `(clojure.lang.MapEntry. ~n (. ~inp-sym ~(:name method)))])) get-methods)
+            nil))
+        (valAt [k#]
+          (case k#
+            ~@(mapcat
+              (fn [method]
+                (let [n (method-name-to-key (:name method))]
+                  [n `(. ~inp-sym ~(:name method))]))
+              get-methods)
+            nil))
+        (seq []
+          (seq ~(vec
+            (for [method get-methods]
+              `(clojure.lang.MapEntry. ~(method-name-to-key (:name method)) (. ~inp-sym ~(:name method)))))))
+        (entrySet []
+          ~(into #{}
+            (for [method get-methods]
+              `(clojure.lang.MapEntry. ~(method-name-to-key (:name method)) (. ~inp-sym ~(:name method))))))
+        (keySet []
+          ~(into #{} (for [method get-methods] (method-name-to-key (:name method)))))
+        (values []
+          ~(->>
+            (for [method get-methods]
+              `(. ~inp-sym ~(:name method)))
+            (vec)))))))
               
 (defmapwrap uri-map java.net.URI)
 ;; (uri-map (java.net.URI. "http://www.google.com/a"))
