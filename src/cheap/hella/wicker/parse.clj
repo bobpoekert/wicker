85a0103bfe3d67d564d448ddcd75c60bb3c1a058
Bob Poekert <bob@poekert.com>
2015-05-03 14:45:54 -0700

    defparser output builds now

--- src/cheap/hella/wicker/parse.clj
+++ src/cheap/hella/wicker/parse.clj
@@ -45,19 +45,20 @@
 
 #_[(:. :*) {:id "nav-flyout-shopAll"} (:div 2) :span (:span {:text category-name})] 
 
-(declare query-form)
+(declare compile-vector-predicate)
 
 (defn- vector-predicate-descend
-  [match-expr node-sym callback var-syms rst]
+  [match-expr node-sym callback rst]
   (let [v-sym (with-meta (gensym "v") {:tag 'org.jsoup.nodes.Node})]
     `(let [^Element v# ~match-expr]
       (if v#
-        (let [^List children# (.childNodes v#)]
+        (let [^java.util.List children# (.childNodes v#)]
           (loop [idx# (dec (.size children#))]
             (if (>= idx# 0)
               (let [~v-sym (.get children# idx#)]
                 (if (instance? Element ~v-sym)
-                  ~(compile-vector-predicate node-sym callback var-syms rst))
+                  (let [~node-sym ~v-sym]
+                    ~(compile-vector-predicate node-sym callback rst)))
                 (recur (dec idx#))))))))))
 
 (defn- expr-error!
@@ -68,8 +69,6 @@
     (str "Invalid expression: ")
     (IllegalArgumentException.)
     (throw)))
-
-(declare compile-vector-predicate)
 
 (defn- var-bind-predicate
   [pred sym node-sym callback rst]
@@ -93,7 +92,15 @@
 
 (defn- kleene-star-predicate
   [raw-pred node-sym callback rst]
-  
+  `(let [kleene-fn# (fn kleene-fn# [~node-sym]
+              ~(compile-vector-predicate node-sym callback (into [raw-pred] rst))
+              (when-let [match# ~(compile-vector-predicate node-sym (fn [v] true) [raw-pred])]
+                (let [^java.util.List children# (.childNodes ~node-sym)]
+                  (loop [idx# (dec (.size children#))]
+                    (when (>= idx# 0)
+                      (kleene-fn# (.get children# idx#))
+                      (recur (dec idx#)))))))]
+    (kleene-fn# ~node-sym)))
 
 (defn- compile-vector-predicate
   [node-sym callback v]
@@ -101,32 +108,47 @@
     (callback node-sym)
     (let [[pred & rst] v]
       (cond
-        (vector? pred) (compile-vector-predicate node-sym (into pred rst)) ;; flatten
-        (= pred :.) (vector-predicate-descend node-sym callback rst)
-        (keyword? pred) (compile-vector-predicate node-sym (into [{:tag-name (name pred)}] rst))
-        (symbol? pred) (vector-predicate-descend (list pred node-sym) callback rst)
+        (vector? pred) (compile-vector-predicate node-sym callback (into pred rst)) ;; flatten
+        (= pred :.) (vector-predicate-descend node-sym node-sym callback rst)
+        (keyword? pred) (compile-vector-predicate node-sym callback (into [{:tag-name (name pred)}] rst))
+        (symbol? pred) (vector-predicate-descend (list pred node-sym) node-sym callback rst)
         (map? pred) (->
-                      `(if (and ~@(u/forcat [[k v] arg]
-                                    (cond
-                                      (string? v) `(= ~(tag-getter node-sym k) ~v)
-                                      (instance? java.util.regex.Pattern v)
-                                        `(re-matches ~v ~(tag-getter node-sym k))
-                                      :else (expr-error! pred))))
+                      `(if (and ~@(->
+                                    (fn [[k v]]
+                                      (cond
+                                        (string? v) `(= ~(tag-getter node-sym k) ~v)
+                                        (instance? java.util.regex.Pattern v)
+                                          `(re-matches ~v ~(tag-getter node-sym k))
+                                        (symbol? v) `(~v ~(tag-getter node-sym k))
+                                        (list? v) (apply list (concat v [(tag-getter node-sym k)]))
+                                        :else (expr-error! pred)))
+                                    (map pred)))
                         ~node-sym)
-                      (vector-predicate-descend callback rst))
+                      (vector-predicate-descend node-sym callback rst))
         (list? pred) (let [[l r] pred]
                       (cond
                         (= r :*) (kleene-star-predicate l node-sym callback rst)
-                        (number? r) (count-descend-prediate l r node-sym callback rst)
+                        (number? r) (count-descend-predicate l r node-sym callback rst)
                         (symbol? r) (var-bind-predicate l r node-sym callback rst)
                         (map? r) (map-var-bind-predicate l r node-sym callback rst)
                         :else (expr-error! pred)))
         :else (expr-error! pred)))))
 
+(defn- gen-query-bindings
+  [page-sym bindings]
+  (let [result-sym (with-meta (gensym "results") {:tag 'java.util.ArrayList})]
+    `(let [~result-sym (java.util.ArrayList.)]
+      ~@(for [[pred & body] bindings]
+        (compile-vector-predicate
+          page-sym
+          (fn [node-sym] `(.add ~result-sym (do ~@body)))
+          pred))
+      ~result-sym)))
+
 (defmacro defparser
   [nom & patterns]
   (let [uri-sym (with-meta (gensym "uri") {:tag 'java.net.URI})
-        page-sym (gensym "parsed-page")]
+        page-sym (with-meta (gensym "parsed-page") {:tag 'org.jsoup.nodes.Element})]
     `(defn ~nom [~uri-sym page#]
       (let [~page-sym (parse-html page#)]
         (clojure.core.match/match (cheap.hella.wicker.util/uri-map ~uri-sym)
@@ -137,6 +159,5 @@
 
 #_(defparser amazon
   ({:path "/"}
-   [(:. :*) {:id "nav-flyout-shopAll"} (:div 2) :span (:span {:text category-name})]
-   category-name))
-   
+   ([(:. :*) {:id "nav-flyout-shopAll"} (:div 2) :span (:span {:text category-name})]
+     category-name)))
